d346b7f486e9f97a72edcccbb133760b184da0f4
Your Name <ahmaddabeer36@gmail.com>
2024-11-07 02:28:40 +0530

    using for to ezy to print all value ak baar ma print

--- LECTURE_9_Vectors/01.cpp
+++ LECTURE_9_Vectors/01.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int main(){
-    vector<int> vec(3,8);
-      cout << vec[0] <<endl;
-      cout << vec[1] <<endl;
-      cout << vec[2] <<endl;
+int main()
+{
+    vector<int> vec (5,0);
+    for(int i : vec){
+        cout << i <<endl;
+    }
     return 0;
 }
