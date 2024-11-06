ad05f604e6049d89f090f0e7ee1918d07e0b617b
Your Name <ahmaddabeer36@gmail.com>
2024-11-07 02:25:14 +0530

    start new chapter vector and this is a syntax

---
+++ LECTURE_9_Vectors/01.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    vector<int> vec(3,8);
+      cout << vec[0] <<endl;
+      cout << vec[1] <<endl;
+      cout << vec[2] <<endl;
+    return 0;
+}
