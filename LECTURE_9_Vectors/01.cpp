cd29da8e879fc0b9a63b2fb7278ae383ee6160d1
Your Name <ahmaddabeer36@gmail.com>
2024-11-07 09:53:29 +0530

    display perticular value of vector using    at

--- LECTURE_9_Vectors/01.cpp
+++ LECTURE_9_Vectors/01.cpp
@@ -3,28 +3,13 @@
 using namespace std;
 int main()
 {
-    vector<int> vec ;
+    vector<int> vec;
 
-    cout <<"size = " << vec.size()<<endl;// ise ham size nikalte ha vector ka
+    cout << "size = " << vec.size() << endl; // ise ham size nikalte ha vector ka
     vec.push_back(25);
     vec.push_back(33);
     vec.push_back(55);
-    cout << "After push back size = "<< vec.size()<<endl;
-    vec.pop_back();//iske use se ham vector ke last se ak value ko delete kar sakte ha 
-
-
-    cout <<vec.front()<<endl;// iska use kar ke ham pahli (front ) value display kara sakte ha vector ki 
-    cout <<vec.back()<<endl;// iska use kar ke ham back(last) value display kara sakte ha vector ki 
-
-   
-
-
-    
-    
-
-    for (int val : vec)// This is syntax for each loop
-    {
-        cout << val << endl;
-    }
-    return 0;
+    cout << vec.at(0) << endl; // iska use kar ke ham vector ki perticular element ko display kara sakte ha
+    cout << vec.at(1) << endl;
+    cout << vec.at(2);
 }
