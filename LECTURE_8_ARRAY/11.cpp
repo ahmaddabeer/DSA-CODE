a8eec738d3e9dfa860351ddceceb79e0d719da19
Your Name <ahmaddabeer36@gmail.com>
2024-11-07 12:55:26 +0530

    time coplexity

--- LECTURE_8_ARRAY/11.cpp
+++ LECTURE_8_ARRAY/11.cpp
@@ -1,2 +1,18 @@
 // Object: write a program in c++ to print all the unique values in an array.
-// DATE:
+// DATE:07|11|2024
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for(int val: nums){
+            ans = ans^val;
+        }
+        return ans;
+        
+    }
+};
+
+
