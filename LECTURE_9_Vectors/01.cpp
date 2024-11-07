6c44c3e0f02f9027ab9f54871903b517b6d89f95
Your Name <ahmaddabeer36@gmail.com>
2024-11-07 09:46:14 +0530

    using pop_back() ka istemal

--- LECTURE_9_Vectors/01.cpp
+++ LECTURE_9_Vectors/01.cpp
@@ -3,11 +3,23 @@
 using namespace std;
 int main()
 {
-    vector<char> vec = { 'a', 'b', 'c','c', 'd'};
+    vector<int> vec ;
 
     cout <<"size = " << vec.size()<<endl;// ise ham size nikalte ha vector ka
+    vec.push_back(25);
+    vec.push_back(33);
+    vec.push_back(55);
+    cout << "After push back size = "<< vec.size()<<endl;
+    vec.pop_back();//iske use se ham vector ke last se ak value ko delete kar sakte ha 
 
-    for (char val : vec)// This is syntax for each loop
+    cout <<vec.back()<<endl;
+   
+
+
+    
+    
+
+    for (int val : vec)// This is syntax for each loop
     {
         cout << val << endl;
     }
