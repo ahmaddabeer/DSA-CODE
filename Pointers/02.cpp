9bfd67672582b1078796f0064892c43a9186d0cf
Your Name <ahmaddabeer36@gmail.com>
2024-11-18 01:15:59 +0530

    pass by reference using pointer

--- Pointers/02.cpp
+++ Pointers/02.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
-void changeA(int a){//pass by value
-a = 20;
+void changeA(int* alpha){//pass by  reference using pointer
+    *alpha = 30;
 
 }
 int main(){
     int a = 10;
-    changeA(a);
+    changeA(&a);
     cout<<"inside main fix : "<< a <<endl; //20
     
     
