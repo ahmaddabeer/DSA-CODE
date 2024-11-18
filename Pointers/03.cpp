ce1089f68118e24c4e7817b128a7cbbe9bbbb126
Your Name <ahmaddabeer36@gmail.com>
2024-11-19 02:28:00 +0530

    additon or subtraction

--- Pointers/03.cpp
+++ Pointers/03.cpp
@@ -2,5 +2,9 @@
 using namespace std;
 
 int  main(){
-    
+    int a = 10;
+    int* alpha = &a;
+    cout << alpha <<endl;//before 
+    alpha = alpha +2;//isme ham nahi add kar rahe balki ham 2 interger ke barabar memori add kar rhe ah 8 byte
+    cout <<alpha <<endl;//After
 }
