59ae9617e7334c79226f392a7e61afe24fe619b0
Your Name <ahmaddabeer36@gmail.com>
2024-11-19 02:29:02 +0530

    delete old code

--- Pointers/03.cpp
+++ Pointers/03.cpp
@@ -2,9 +2,5 @@
 using namespace std;
 
 int  main(){
-    int a = 10;
-    int* alpha = &a;
-    cout << alpha <<endl;//before 
-    alpha = alpha +2;//isme ham nahi add kar rahe balki ham 2 interger ke barabar memori add kar rhe ah 8 byte
-    cout <<alpha <<endl;//After
+    
 }
