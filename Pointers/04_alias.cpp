7959c8e6ceeee63b4ed4241ed4387513d7d8b986
Your Name <ahmaddabeer36@gmail.com>
2024-11-20 03:02:30 +0530

    alias use

---
+++ Pointers/04_alias.cpp
@@ -0,0 +1,13 @@
+// ab jase pichle code ma dekha ki pass by refrence using pointer ki help se ham add ma apni man cahi value ko add kar sakte ha usi tarah ha ab alias pare gay
+#include <iostream>
+using namespace std;
+void alias(int &b)
+{ // ya jo  ham b liye ha ye a ka dusra naam ha but ham baat a ki hi kar rahe ha
+    b = 200;
+}
+int main()
+{
+    int a = 23;
+    alias(a);
+    cout << "you exepet the value of a is 23 but ans is no and the value of a is : " << a << endl;
+}
