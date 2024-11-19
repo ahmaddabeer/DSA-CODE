486b78130340e927901379a8299bd1539ea33fdd
Your Name <ahmaddabeer36@gmail.com>
2024-11-20 03:19:25 +0530

    using pointer acces the any arr value

--- Pointers/04_alias.cpp
+++ Pointers/04_alias.cpp
@@ -1,7 +1,7 @@
 // ab jase pichle code ma dekha ki pass by refrence using pointer ki help se ham add ma apni man cahi value ko add kar sakte ha usi tarah ha ab alias pare gay
 #include <iostream>
 using namespace std;
-void alias(int &b)
+void alias(int &b)//pass by reference using alias
 { // ya jo  ham b liye ha ye a ka dusra naam ha but ham baat a ki hi kar rahe ha
     b = 200;
 }
